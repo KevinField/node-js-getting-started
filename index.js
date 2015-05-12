5037f6926bc0ea8851b9c59dda2be51d21551e27
Kevin Field <kevinjamesfield@gmail.com>
2015-05-12 13:04:35 -0400

    Added ping.

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', function(request, response) {
   response.send('<h1>Hello Yose</h1>');
 });
 
+app.get('/ping', function(request, response) {
+	response.writeHead(200, { 'Content-Type': 'application/json'});
+    response.end(JSON.stringify({'alive':true}));
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
