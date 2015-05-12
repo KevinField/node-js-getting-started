2987d7720e02b82cba97281d14eaf14623de6248
Kevin Field <kevinjamesfield@gmail.com>
2015-05-12 13:23:17 -0400

    Whoops, wanted both.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('<a href="https://github.com/KevinField/node-js-getting-started/#readme" id="repository-link">my repo</a>');
+  response.send('<h1>Hello Yose</h1><p><a href="https://github.com/KevinField/node-js-getting-started/#readme" id="repository-link">my repo</a></p>');
 });
 
 app.get('/ping', function(request, response) {
