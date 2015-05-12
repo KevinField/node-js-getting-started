0496f204907f5e0051a203d73c474d844d3b21d6
Kevin Field <kevinjamesfield@gmail.com>
2015-05-12 17:56:36 -0400

    Add fun faces.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var faces = require("cool-ascii-faces");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('<h1>Hello Yose</h1><p><a href="https://github.com/KevinField/node-js-getting-started/#readme" id="repository-link">my repo</a></p>');
+  response.send('<h1>Hello Yose</h1><p><a href="https://github.com/KevinField/node-js-getting-started/#readme" id="repository-link">my repo</a></p><pre>'+faces()+'</pre>');
 });
 
 app.get('/ping', function(request, response) {
