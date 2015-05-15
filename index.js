fd9276fd8a7f2f864431acb429844b39e255bd39
Kevin Field <kevinjamesfield@gmail.com>
2015-05-15 10:34:27 -0400

    World 2 level 1.

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.get('/', function(request, response) {
   var times = process.env.TIMES || 5
   for (i=0; i < times; i++)
     result += faces() + '\n';
-  response.send('<h1>Hello Yose</h1><p><a href="https://github.com/KevinField/node-js-getting-started/#readme" id="repository-link">my repo</a></p><pre>Some faces:\n'+result+'</pre>');
+  response.send('<h1>Hello Yose</h1><p><a href="https://github.com/KevinField/node-js-getting-started/#readme" id="repository-link">my repo</a></p><pre>Some faces:\n'+result+'</pre>'
+  	+"<p>Also, you can <a href='http://kevinjamesfield.blogspot.com/' id='contact-me-link'>contact me</a>.</p>"	
+  );
 });
 
 app.get('/ping', function(request, response) {
