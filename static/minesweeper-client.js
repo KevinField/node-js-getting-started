11e31e626f2d5d238bde49155dcef7807c490fa5
Kevin Field <kevinjamesfield@gmail.com>
2015-05-15 20:08:39 -0400

    Failing now, not sure why...trying without debug code, even though that didn't seem to get in the way earlier.

--- static/minesweeper-client.js
+++ static/minesweeper-client.js
@@ -60,17 +60,4 @@ var dummy = function(){
 		_('grid').innerHTML='';
 		_('grid').appendChild(docfrag);
 	};
-	window.addEventListener('DOMContentLoaded',function(){
-		document.grid = [
-			['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-			['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-			['empty', 'empty', 'empty', 'empty', 'empty', 'bomb' , 'empty', 'empty'],
-			['empty', 'empty', 'empty', 'empty', 'bomb' , 'empty', 'empty', 'empty'],
-			['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-			['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-			['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-			['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'],
-		];
-		load();
-	});
 }();
