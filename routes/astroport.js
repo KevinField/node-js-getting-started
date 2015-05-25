2b579cb80544289bc7881dbd2eabd5f842fc1f2a
Kevin Field <kevinjamesfield@gmail.com>
2015-05-24 21:20:16 -0400

    World 6, level 2, plus a tiny bit of thus-far-unused testing setup.

--- routes/astroport.js
+++ routes/astroport.js
@@ -1,3 +1,6 @@
 'use strict';module.exports = {'/astroport': function(req, res){
-	res.send('<h1 id=astroport-name>Yoyo</h1>');
+	var gatesnships = ['gates','ships'].map(function(f){return [1,2,3].map(function(e){return '<span id='+f+'-'+e+'></span>';}).join('')}).join('');
+	res.send('<h1 id=astroport-name>Yoyo</h1>'
+	+gatesnships
+);
 }};
