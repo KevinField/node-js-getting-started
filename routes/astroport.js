f81f43e76d2eab53d67c27fc3a70a4aea915c7f9
Kevin Field <kevinjamesfield@gmail.com>
2015-05-24 21:25:47 -0400

    Syntax error fix.

--- routes/astroport.js
+++ routes/astroport.js
@@ -1,5 +1,5 @@
 'use strict';module.exports = {'/astroport': function(req, res){
-	var gatesnships = [1,2,3].map(function(e){return '<span id=gate-'+e+'><span id=ship-'+e+'></span></span>';}).join('')});
+	var gatesnships = [1,2,3].map(function(e){return '<span id=gate-'+e+'><span id=ship-'+e+'></span></span>';}).join('');
 	res.send('<h1 id=astroport-name>Yoyo</h1>'
 	+gatesnships
 );
