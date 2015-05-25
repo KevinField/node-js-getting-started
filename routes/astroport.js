de7b65664cc3e7f01302b01c9414a7c18d5757ae
Kevin Field <kevinjamesfield@gmail.com>
2015-05-24 21:24:35 -0400

    Ship needs to be in gate.

--- routes/astroport.js
+++ routes/astroport.js
@@ -1,5 +1,5 @@
 'use strict';module.exports = {'/astroport': function(req, res){
-	var gatesnships = ['gate','ship'].map(function(f){return [1,2,3].map(function(e){return '<span id='+f+'-'+e+'></span>';}).join('')}).join('');
+	var gatesnships = [1,2,3].map(function(e){return '<span id=gate-'+e+'><span id=ship-'+e+'></span></span>';}).join('')});
 	res.send('<h1 id=astroport-name>Yoyo</h1>'
 	+gatesnships
 );
